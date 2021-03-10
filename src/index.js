c1b20ce8c146fbdff33b3e95c407e36e69a47318
Andy Prasetya <andyprasetya@aol.com>
2021-03-10 14:21:46 +0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const worker = new Worker(new URL('./main-worker.worker.js', import.meta.url));
+worker.postMessage({
+  question:
+    'The Answer to the Ultimate Question of Life, The Universe, and Everything.',
+});
+worker.onmessage = ({ data: { answer } }) => {
+  console.log(answer);
+};
